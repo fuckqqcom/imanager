f611e2470381e34e27efd0f14f82afd71c5211cb
95028555@qq.com <yangyi199159>
2018-12-23 09:30:55 +0800

    init

---
+++ src/pages/demo/life.js
@@ -0,0 +1,13 @@
+import React , { Component } from "react"
+import { Button } from "antd"
+// import "../../App.css"
+export default class Life extends  Component{
+    render(){
+        return (
+            <div>
+            <div>Life</div>
+            <Button>点击一下</Button>
+            </div>
+        )
+    }
+}
